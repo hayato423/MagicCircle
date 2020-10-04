d79734b2299e37c3288d5163404b10fc6b930166
hayato423 <49159244+hayato423@users.noreply.github.com>
2020-10-05 06:25:52 +0900

    add .gitignore

--- ShibaLab/settings.py
+++ ShibaLab/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'magic_circle.apps.MagicCircleConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
